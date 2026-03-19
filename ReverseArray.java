53167349a95c762c9296616baf1318995d5a7470
simpi2007 <simpiyadav720@gmail.com>
2026-03-19 12:29:45 +0530

    My 4th commit

---
+++ ReverseArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Array elements are:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Reverse is:");
+        for (int i = size - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+            
+        }
+        
+
+    }
+
+}
